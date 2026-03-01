dbef95eb8ae0e14b974ca06fc0100ac3ad48b706
LucasSantos <lucas2005876@gmail.com>
2026-03-01 13:14:56 -0300

    primeiro commit

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,11 +1,25 @@
-import { CardPost } from "../../components/CardPost"
-import { posts } from "./data"
-import styles from './feed.module.css'
+import { useEffect, useState } from "react";
+import { CardPost } from "../../components/CardPost";
+import styles from "./feed.module.css";
 
 export const Feed = () => {
-    return (
-        <main className={styles.grid}>
-            {posts.map(post => <CardPost key={post.slug} post={post} />)}
-        </main>
-    )
-}
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/blog-posts")
+      .then((respostaApi) => {
+        return respostaApi.json();
+      })
+      .then((resposta) => {
+        setPosts(resposta);
+      });
+  }, []);
+
+  return (
+    <main className={styles.grid}>
+      {posts.map((post) => (
+        <CardPost key={post.slug} post={post} />
+      ))}
+    </main>
+  );
+};
